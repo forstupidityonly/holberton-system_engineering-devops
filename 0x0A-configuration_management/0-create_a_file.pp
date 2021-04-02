8683e81e3cf9fdf256bdaebe979cced48020d52a
Corbin <forstupidityonly@gmail.com>
2021-04-02 18:41:40 -0500

    carlos was talking about taking off the quotes so i tried it

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file
 file { '/tmp/holberton':
-  ensure  => 'file'
-  group   => 'www-data',
-  owner   => 'www-data',
+  ensure  => file,
+  group   => www-data,
+  owner   => www-data,
   mode    => '0744',
   content => 'I love Puppet',
 }
