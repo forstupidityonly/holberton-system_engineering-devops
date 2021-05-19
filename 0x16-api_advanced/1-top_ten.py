bd4784ed04d704e2cff6e935c28e42b0528150d3
Corbin <forstupidityonly@gmail.com>
2021-05-19 13:41:50 -0500

    hot

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import sys
 
 def top_ten(subreddit):
     """top ten post"""
-    url = "https://www.reddit.com/r/{}/top/.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     sub = requests.get(url, headers={"User-Agent": "Custom"})
     if (sub.status_code == 200):
         val = sub.json().get("data").get("children")
