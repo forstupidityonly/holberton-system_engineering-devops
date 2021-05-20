e0c530047ad143e1c0e017c055543acaa5d759e7
Corbin <forstupidityonly@gmail.com>
2021-05-19 23:56:45 -0500

    pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,18 +3,23 @@
 import requests
 import sys
 
+
 def recurse(subreddit, hot_list=[], counter=None):
     """same thing but recursive"""
     parameters = {"limit": 100}
     url = "https://www.reddit.com/r/" + str(subreddit) + "/hot/.json?counter"
-           + str(counter)
-    sub = requests.get(url, headers={"User-Agent": "Custom"}, params=parameters,
-                    allow_redirects=False)
+    + str(counter)
+    sub = requests.get(
+        url,
+        headers={
+            "User-Agent": "Custom"},
+        params=parameters,
+        allow_redirects=False)
     if (sub.status_code == 200):
         post_list = []
         hot_post = r.json().get("data").get("children")
         for post in hot_post:
-           hot_list.append(post.get("data").get("title"))
+            hot_list.append(post.get("data").get("title"))
         counter = (r.json().get("data").get("counter"))
         if counter is None:
             return (hot_list)
